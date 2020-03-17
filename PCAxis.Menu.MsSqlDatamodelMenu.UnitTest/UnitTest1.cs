62f33726b09ef5971cc790b19502ac4c0c737954
cklinton <60175636+cklinton@users.noreply.github.com>
2020-03-17 16:53:22 +0100

    Added a dummy unit test

--- PCAxis.Menu.MsSqlDatamodelMenu.UnitTest/UnitTest1.cs
+++ PCAxis.Menu.MsSqlDatamodelMenu.UnitTest/UnitTest1.cs
@@ -8,6 +8,16 @@ namespace PCAxis.Menu.MsSqlDatamodelMenu.UnitTest
 		[TestMethod]
 		public void TestMethod1()
 		{
+			//Arrange
+			var a = 0;
+			var b = 0;
+
+			//Act
+
+			//Assert
+			//Check their equality.
+			Assert.AreEqual(a, b);
+
 		}
 	}
 }
